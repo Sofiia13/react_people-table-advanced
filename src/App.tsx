c79d487242567d0f69e3f7c17ee5edc3f81e54c9
Sofiia13 <sophiyastanish@gmail.com>
2025-08-07 22:52:57 +0300

    move home page to a separate component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Navbar } from './components/Navbar';
 
 import './App.scss';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { HomePage } from './components/HomePage';
 
 export const App = () => {
   return (
@@ -12,7 +13,7 @@ export const App = () => {
       <div className="section">
         <div className="container">
           <Routes>
-            <Route path="/" element={<h1 className="title">Home Page</h1>} />
+            <Route path="/" element={<HomePage />} />
             <Route path="/home" element={<Navigate to="/" replace />} />
 
             <Route path="/people" element={<PeoplePage />} />
